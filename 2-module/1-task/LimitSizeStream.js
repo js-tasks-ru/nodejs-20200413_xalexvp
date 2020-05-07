0c53f51bbb216a1b25405667b84d2706e24fb54d
aonishchenko <aonishchenko@koiconsults.com>
2020-05-08 02:17:30 +0300

    2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -1,13 +1,20 @@
 const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
+let counter = 0;
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.options = options;
   }
 
   _transform(chunk, encoding, callback) {
-
+    counter += chunk.length;
+    if (counter > this.options.limit) {
+      callback( new LimitExceededError, chunk);
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
